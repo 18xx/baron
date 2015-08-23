8cb32ac5bd7a96bead7460749a8eca5fb3f5f3ba
Gregor MacDougall <gregor@freerunningtechnologies.com>
2015-08-23 11:16:15 -0400

    Add check for major company floatation

--- lib/baron/company/major_company.rb
+++ lib/baron/company/major_company.rb
@@ -5,6 +5,21 @@ module Baron
     # and eight standard certificates each representing a 10% stake
     class MajorCompany < self
       include Shareholder
+
+      # Has this company floated?
+      #
+      # Once a company has sold a particular threshold of certificates, the
+      # company is floated, and begins operating. The first thing that happens
+      # when a company floats is that it is given is operating capital.
+      #
+      # @example
+      #   company.floated?
+      #
+      # @api public
+      # @return [Boolean]
+      def floated?
+        transactions.any?
+      end
     end
   end
 end
--- spec/unit/baron/company/major_company_spec.rb
+++ spec/unit/baron/company/major_company_spec.rb
@@ -1,23 +1,45 @@
 RSpec.describe Baron::Company::MajorCompany do
-  subject { described_class.new(abbreviation, name) }
   let(:abbreviation) { 'LNWR' }
+  let(:company) { described_class.new(abbreviation, name) }
   let(:name) { 'London and North Western Railway' }
 
   describe '#abbrevation' do
+    subject { company.abbreviation }
     it 'returns the company abbreviation' do
-      expect(subject.abbreviation).to eq 'LNWR'
+      should eq 'LNWR'
     end
   end
 
   describe '#name' do
+    subject { company.name }
     it 'returns the company name' do
-      expect(subject.name).to eq 'London and North Western Railway'
+      should eq 'London and North Western Railway'
     end
   end
 
   describe '#to_s' do
+    subject { company.to_s }
     it 'returns the company abbreviation' do
-      expect(subject.to_s).to eq 'LNWR'
+      should eq 'LNWR'
+    end
+  end
+
+  describe '#floated?' do
+    subject { company.floated? }
+    context 'when the company has no transactions' do
+      it { should be false }
+    end
+
+    context 'when the company has a transaction' do
+      before do
+        Baron::Transaction.new(
+          company,
+          [Baron::Money.new(1000)],
+          nil,
+          []
+        )
+      end
+      it { should be true }
     end
   end
 end
